308851b2b80dac3d1e62d75c43a34c339262b861
Jared Krause <kravse@users.noreply.github.com>
2022-11-17 18:01:06 -0500

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,15 +1,19 @@
-(() => {let list = []; let timeout; addEventListener('scroll', (event) => {
-  timeout && clearTimeout(timeout)
-  list = [
-    ...new Set(
-      [
-        ...document.querySelectorAll(
-          '[aria-labelledby="accessible-list-0"] [role="link"]'
-        ),
-      ].map((val) => val.href).concat(list)
-    ),
-  ].filter(val => !val.includes('?') && val.includes('https://twitter.com/'));
-  timeout = setTimeout(() => {
-    console.log(list.join(',\n'));
-  }, 2500)
-})})()
+(() => {
+  let list = [];
+  let timeout;
+  addEventListener("scroll", () => {
+    timeout && clearTimeout(timeout);
+    list = [
+      ...new Set(
+        [...document.querySelectorAll('main section [role="link"]')]
+          .map((val) => val?.href || "")
+          .concat(list)
+      ),
+    ].filter(
+      (val) => !!val && !val.includes("?") && val.includes("https://twitter.com")
+    );
+    timeout = setTimeout(() => {
+      console.log(list.join(",\n"));
+    }, 2500);
+  });
+})();
